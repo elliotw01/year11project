82734781450cbb800cd155868214cc3a1fbcacd5
leadtrip <mike.wood@weareact.com>
2020-12-05 16:15:52 +0000

    Further work on project

--- src/main/java/wood/elliot/AppMain.java
+++ src/main/java/wood/elliot/AppMain.java
@@ -1,38 +1,42 @@
 package wood.elliot;
 
 import java.sql.*;
+import java.util.Scanner;
 
 public class AppMain {
 
-    private static final String DEFAULT_USERNAME = "elliotw";
-    private static final String DEFAULT_PASSWORD = "3Ll10Tw";
-
     public static void main(String[] args) {
         try {
-            Class.forName("org.h2.Driver");
-            Connection conn = DriverManager.getConnection( "jdbc:h2:./ewoodyr11DB" );
-            Statement statement = conn.createStatement();
-            statement.execute( "create table if not exists users(username varchar(10), password varchar(20))" );
-            loadUsers( conn );
+            createDatabase();
 
-            new ArtistAndSong().populateMap();
+            try (Scanner scanner = new Scanner(System.in)) {
+                System.out.println( "Press 1 to play or 2 to register" );
+                String option = scanner.nextLine();
+
+                if( option == "1" ) {
+                    System.out.println("1 entered");
+                }
+                else if ( option == "2" ) {
+                    System.out.println("2 entered");
+                }
+                else {
+                    System.out.println("entered: " + option );
+                }
+
+                System.out.print("Enter your username : ");
+                String name = scanner.nextLine();
+            }
         }
         catch ( Exception e ) {
             e.printStackTrace();
         }
     }
 
-    private static void loadUsers( Connection conn ) throws SQLException {
-        PreparedStatement ps1 = conn.prepareStatement( "select count(*) from users where username = ?" );
-        ps1.setString( 1, DEFAULT_USERNAME );
-        ResultSet rs = ps1.executeQuery();
-        rs.next();
-        if( rs.getInt(1) != 1 ) {
-            System.out.println( "Adding default user" );
-            PreparedStatement ps2 = conn.prepareStatement( "insert into users ( username, password ) values( ?, ? )" );
-            ps2.setString( 1, DEFAULT_USERNAME );
-            ps2.setString( 2, DEFAULT_PASSWORD );
-            ps2.execute();
-        }
+    private static void createDatabase() throws ClassNotFoundException, SQLException {
+        Class.forName("org.h2.Driver");
+        Connection conn = DriverManager.getConnection( "jdbc:h2:./ewoodyr11DB" );
+        Statement statement = conn.createStatement();
+        statement.execute( "create table if not exists users(username varchar(10))" );
     }
+
 }

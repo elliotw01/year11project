fd8cae3cca573f429505dcaa886ba4b7b27b36e1
leadtrip <mike.wood@weareact.com>
2020-12-10 20:38:57 +0000

    Further work on project

--- src/main/java/wood/elliot/AppMain.java
+++ src/main/java/wood/elliot/AppMain.java
@@ -10,15 +10,26 @@ public class AppMain {
     public static final int MAX_OPTION_ATTEMPTS = 10;
     public static final int MAX_AUTHENTICATE_ATTEMPTS = 3;
     public static final int EXIT_GAME = 0;
+    private static final String CREATE_USER_TABLE = "create table if not exists users(username varchar(10))";
+    private static final String FIND_USER = "select * from users where username = ?";
+    private static final String ADD_USER = "insert into users (username) values( ? )";
+
+    private Connection conn;
+    private Scanner scanner;
 
     public static void main(String[] args) {
+        new AppMain();
+    }
+
+    public AppMain() {
         try {
+            scanner = new Scanner(System.in);
+            createConnection();
             createDatabase();
-            AppMain appMain = new AppMain();
-            Integer registerOrPlay = appMain.registerOrPlay();
+            Integer registerOrPlay = registerOrPlay();
             switch (registerOrPlay.toString()) {
-                case PLAY -> appMain.startGame();
-                case REGISTER -> appMain.register();
+                case PLAY -> startGame();
+                case REGISTER -> register();
                 default -> System.out.println("Too many chances, quitting");
             }
         }
@@ -28,52 +39,71 @@ public class AppMain {
     }
 
     private Integer registerOrPlay() {
-        try (Scanner scanner = new Scanner(System.in)) {
-            for(int i = 0; i < MAX_OPTION_ATTEMPTS; i++ ) {
-                System.out.println("Press 1 to play or 2 to register");
-                String option = scanner.nextLine();
-                if (option.equals(PLAY) || option.equals(REGISTER)) {
-                    return Integer.valueOf( option );
-                } else {
-                    System.out.println( "Unrecognised value entered '" + option + "'" );
-                }
+        for(int i = 0; i < MAX_OPTION_ATTEMPTS; i++ ) {
+            System.out.println("Press 1 to play or 2 to register");
+            String option = scanner.nextLine();
+            if (option.equals(PLAY) || option.equals(REGISTER)) {
+                return Integer.valueOf( option );
+            } else {
+                System.out.println( "Unrecognised value entered '" + option + "'" );
             }
         }
         return EXIT_GAME;
     }
 
-    private void startGame() {
+    private void startGame() throws Exception{
         System.out.println( "Starting game" );
-        authenticate();
+        String username = authenticate();
+        if( username != null ) {
+            new SongGame( scanner, username ).playGame();
+        }
     }
 
-    private String authenticate() {
+    private String authenticate() throws SQLException {
         System.out.println( "Authenticating" );
-        try (Scanner scanner = new Scanner(System.in)) {
-            for(int i = 0; i < MAX_AUTHENTICATE_ATTEMPTS; i++ ) {
-                System.out.println("Please enter your username");
-                String username = scanner.nextLine();
-                if( userExists( username ) ) {
-                    return username;
-                }
+        System.out.println("Please enter your username");
+        for(int i = 0; i < MAX_AUTHENTICATE_ATTEMPTS; i++ ) {
+            String username = scanner.next();
+            if( userExists( username ) ) {
+                System.out.println( "Welcome back " + username );
+                return username;
+            }
+            else {
+                System.out.println( "User not found, you have " + ( MAX_AUTHENTICATE_ATTEMPTS - (i+1) ) + " attempts remaining" );
             }
         }
-        throw new UnsupportedOperationException( "User does not exist" );
+        return null;
     }
 
-    private boolean userExists( String username ) {
-        return false;
+    private boolean userExists( String username ) throws SQLException {
+        PreparedStatement statement = conn.prepareStatement( FIND_USER );
+        statement.setString( 1,  username );
+        return statement.executeQuery().next();
     }
 
-    private void register() {
+    private void register() throws Exception {
         System.out.println( "Registering" );
+        System.out.println( "Enter your username" );
+        String username = scanner.next();
+        if( userExists( username ) ) {
+            System.out.println( "User already exists" );
+        }
+        else {
+            PreparedStatement statement = conn.prepareStatement( ADD_USER );
+            statement.setString( 1, username );
+            statement.execute();
+            System.out.println( "User registered" );
+        }
+        startGame();
     }
 
-    private static void createDatabase() throws ClassNotFoundException, SQLException {
+    private void createConnection() throws ClassNotFoundException, SQLException {
         Class.forName("org.h2.Driver");
-        Connection conn = DriverManager.getConnection( "jdbc:h2:./ewoodyr11DB" );
-        Statement statement = conn.createStatement();
-        statement.execute( "create table if not exists users(username varchar(10))" );
+        conn = DriverManager.getConnection( "jdbc:h2:./ewoodyr11DB" );
     }
 
+    private void createDatabase() throws SQLException {
+        Statement statement = conn.createStatement();
+        statement.execute( CREATE_USER_TABLE );
+    }
 }
--- src/main/java/wood/elliot/SongGame.java
+++ src/main/java/wood/elliot/SongGame.java
@@ -1,9 +1,41 @@
 package wood.elliot;
 
+import java.io.File;
+import java.nio.file.Files;
+import java.util.*;
+import java.util.stream.Stream;
+
 public class SongGame {
 
-    public void playGame() {
-        
+    private final static String SONG_FILE = "songs.txt";
+    private final static String RESULTS_FILE = "results.txt";
+    public static final String DELIMITER = "=";
+
+    Scanner scanner;
+    String username;
+    Map<String, String> songs = new HashMap<>();
+
+    public SongGame( Scanner scan, String user ) throws Exception {
+        scanner = scan;
+        username = user;
+        loadSongs();
+    }
+
+    private void loadSongs() throws Exception{
+        String rootPath = getClass().getClassLoader().getResource( "" ).getPath();
+        File file = new File( rootPath + SONG_FILE );
+
+        try (Stream<String> lines = Files.lines( file.toPath() ) ) {
+            lines.filter(line -> line.contains(DELIMITER)).forEach(
+                    line -> songs.putIfAbsent(line.split(DELIMITER)[0], line.split(DELIMITER)[1])
+            );
+        }
+    }
+
+    public void playGame( ) {
+        System.out.println( "Playing game" );
+        int songNumber = new Random().nextInt( songs.size() );
+
     }
 
 }
